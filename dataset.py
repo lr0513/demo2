f80cb694a464afb809bbe53d0bb84c52441a9d25
lorinda <15941174592@163.com>
2026-09-04 14:12:07 +0800

    补充get_collate_fn说明

--- dataset.py
+++ dataset.py
@@ -78,30 +78,39 @@ class NERDataset(Dataset):
 
 
 def get_collate_fn(tokenizer, max_len):
+    '''
+    给每个原始字找到它在BERT token序列中的位置，再把该字的标签放到对应位置，特殊token和非首个sub-word统一用-100忽略
+    :param tokenizer:
+    :param max_len:
+    :return: 
+    '''
     def collate_fn(batch):
         texts = [item[0] for item in batch]
         batch_labels = [item[1] for item in batch]
 
-        # 关键点：is_split_into_words=True，输入是单字列表！
         encode_result = tokenizer(
             texts,
-            padding="max_length",
+            padding="max_length", # 保证batch内形状一致
             truncation=True,
             return_tensors="pt",
             max_length=max_len,
-            is_split_into_words=True
+            is_split_into_words=True # 告知分词器输入已经是拆分完成的字符序列，不再重新切分文本
         )
 
         input_ids = encode_result["input_ids"]
         attention_mask = encode_result["attention_mask"]
         batch_size, seq_len = input_ids.shape
 
-        label_tensor = torch.full((batch_size, seq_len), fill_value=-100, dtype=torch.long)
+        label_tensor = torch.full(
+            (batch_size, seq_len),
+            fill_value=-100,
+            dtype=torch.long
+        )
 
         for i in range(batch_size):
             raw_label = batch_labels[i]
-            word_id_list = encode_result.word_ids(batch_index=i)
-            prev_word_idx = None
+            word_id_list = encode_result.word_ids(batch_index=i) # 第i条样本中，每一个BERT token对应原始输入列表的下标
+            prev_word_idx = None # 记录上一个token属于原始句子的哪个词，用于处理“一个原始词被tokenizer切成了多个token”的情况
             for pos, word_idx in enumerate(word_id_list):
                 if word_idx is None:
                     # CLS / SEP / PAD
